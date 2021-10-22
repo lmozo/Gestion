9c953ad8ca664dc8fb7ac416978bf7078a5c1d96
sandramgs <sandramgs7@gmail.com>
2021-10-21 20:27:40 -0500

    cambios sandra

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, request
 from flask.templating import render_template
-from forms import FormEmpleado, FormLogin
+from forms import FormEmpleado, FormLogin, FormUsuario
 from listas import lista_usuarios, lista_empleados
 from forms import FormRegistro
 # FormCrearEmadople,
@@ -11,13 +11,6 @@ app = Flask(__name__)
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 
-"""@app.route("/")
-def index():
-    return render_template('index.html')
-
-@app.route("/")
-def logins():
-    return render_template('login.html')"""
 
 @app.route('/', methods=["GET", "POST"])
 def login():
@@ -31,44 +24,13 @@ def login():
         if formulario.validate_on_submit():
             if validar_login(formulario.usuario.data,formulario.password.data):
                 mensaje = "El usuario {0} inició sesión".format(formulario.usuario.data)
-                return render_template('Index.html',mensaje=mensaje)
+                return render_template('Index.html',form=FormLogin(),mensaje=mensaje)
             else:
                 mensaje = "Los datos ingresados NO son válidos."
                
         mensaje += "Todos los datos son requeridos"
-        return render_template('index.html',form=FormLogin(), mensaje=mensaje)
+        return render_template('index.html',form=formulario, mensaje=mensaje)
 
-@app.route("/crear_usuario/")
-def crear_u():
-    return render_template('crear_usuario.html')
-
-@app.route("/editar_usuario/")
-def editar_u():
-    return render_template('editar_usuario.html')
-
-@app.route("/consultar_usuario/")
-def consultar_u():
-    return render_template('consultar_usuario.html')
-
-@app.route("/crear_empleado/")
-def crear_e():
-    return render_template('crear_empleado.html')
-
-@app.route("/editar_empleado/")
-def editar_e():
-    return render_template('editar_empleado.html')
-
-@app.route("/consultar_empleado/")
-def consultar_e():
-    return render_template('consultar_empleado.html')
-
-@app.route("/evaluacion/")
-def evaluacion():
-    return render_template('evaluacion.html')
-
-@app.route("/empleado_evaluar/")
-def empleado_evaluar():
-    return render_template('empleado_evaluar.html')
 
 @app.route('/registro/', methods=('GET', 'POST'))
 def registro():
@@ -96,8 +58,72 @@ def registro():
         else:
             return render_template('registro.html',form=formulario, mensaje = mensaje)
 
-@app.route('/AdministracionEmpleados/CrearEmpleado', methods=('GET', 'POST'))
-def crearEmpleado():
+
+@app.route("/crear_usuario/")
+def crear_u():
+    mensaje = ""
+    if request.method == "GET": 
+        formulario = FormUsuario()
+        return render_template('crear_usuario.html', form = formulario)
+    else:
+        formulario = FormUsuario(request.form)
+        if formulario.validate_on_submit():
+            if registrar_usuario(formulario.data):
+                 mensaje += "Usuario registrado exitosamente."
+            else:
+                mensaje += "Ocurrió un error durante el registro del usuario, por favor intente nuevamente."
+        else:
+            mensaje += "Todos los datos son requeridos."
+        
+        return render_template('crear_usuario.html',form=formulario, mensaje = mensaje)
+
+
+@app.route("/editar_usuario/")
+def editar_u():
+    mensaje = ""
+    if request.method == "GET": 
+        formulario = FormEditarUsuario()
+        return render_template('editar_usuario.html', form = formulario)
+    else:
+        formulario = FormEditarUsuario(request.form)
+        if formulario.validate_on_submit():
+            if editar_usuario(formulario.data):
+                 mensaje += "Usuario editado exitosamente."
+            else:
+                mensaje += "Ocurrió un error durante la edición del usuario, por favor intente nuevamente."
+        else:
+            mensaje += "Todos los datos son requeridos."
+        
+        return render_template('editar_usuario.html',form=formulario, mensaje = mensaje)
+
+
+@app.route("/consultar_usuario/")
+def consultar_u():
+    return render_template('consultar_usuario.html')
+
+@app.route("/crear_empleado/")
+def crear_e():
+    return render_template('crear_empleado.html')
+
+@app.route("/editar_empleado/")
+def editar_e():
+    return render_template('editar_empleado.html')
+
+@app.route("/consultar_empleado/")
+def consultar_e():
+    return render_template('consultar_empleado.html')
+
+@app.route("/evaluacion/")
+def evaluacion():
+    return render_template('evaluacion.html')
+
+@app.route("/empleado_evaluar/")
+def empleado_evaluar():
+    return render_template('empleado_evaluar.html')
+
+
+@app.route('/admin_empleados/crear_empleado/', methods=('GET', 'POST'))
+def crear_empleado():
     mensaje = ""
     if request.method == "GET": 
         formulario = FormEmpleado()
@@ -113,6 +139,7 @@ def crearEmpleado():
             mensaje += "Todos los datos son requeridos."
         
         return render_template('crear_empleado.html',form=formulario, mensaje = mensaje)
+
 
 def validar_login(usuario,password):
     for i in range(len(lista_usuarios)):
